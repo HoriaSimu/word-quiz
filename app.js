635c17975717cc3201268a244407f619d7e725ca
Horia Simu <simu.horia@gmail.com>
2018-04-15 22:51:31 +0300

    Started mock-up of app. Reused stylesheet from previous project.

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var ImageDivStyles = {
+  display: "inline-block",
+  backgroundImage: "url('img/placeholder.png')" }; // to get rid of this variable later
 
-ReactDOM.render(<h1>Hello from React</h1>, document);
+function Application() {
+  return (
+    <div id="Window">
+      <div id="progressDiv">
+        <div id="progressBar">
+          <span></span>
+        </div>
+      </div>
+      <div id="imageDiv" style={ImageDivStyles}></div>
+      <div id="answerDiv">
+        <label id="firstLine">c is for...</label>
+        <label className="letter">c</label>
+        <input className="inputLetter" />
+        <input className="inputLetter" />
+        <input className="inputLetter" />
+        <input className="inputLetter" />
+        <input className="inputLetter" />
+        <input className="inputLetter" />
+        <label className="letter">r</label>
+      </div>
+      <div id="buttonsDiv">
+        <label id="messageCorrect">Correct!</label>
+        <a id="checkAnswerButton">Check answer</a>
+        <a id="skipQuestionButton">Skip question</a>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<Application />, document.getElementsByTagName("main")[0]);
