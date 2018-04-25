e5f522669cd82cd0f905d372a2a87c95183732b8
Horia Simu <simu.horia@gmail.com>
2018-04-25 21:57:30 +0300

    Broke the app down into several stateful components

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 var ImageDivStyles = {
   display: "inline-block",
   backgroundImage: "url('img/placeholder.png')" }; // to get rid of this variable later
 
-function Application() {
-  return (
-    <div id="window">
+const wordlist = ["apple", "horse", "spoon"];
+
+class Header extends React.Component {
+  render()  {
+    return (
       <div id="progressDiv">
         <div id="progressBar">
           <span></span>
         </div>
       </div>
+    );
+  }
+};
+
+class Image extends React.Component {
+  render() {
+    return (
       <div id="imageDiv" style={ImageDivStyles}></div>
+    );
+  }
+};
+
+class Word extends React.Component {
+  render() {
+    return (
       <div id="answerDiv">
         <label id="firstLine">c is for...</label>
         <label className="letter">c</label>
@@ -25,13 +42,51 @@ function Application() {
         <input className="inputLetter" />
         <label className="letter">r</label>
       </div>
+    );
+  }
+};
+
+Word.propTypes = {
+  word: PropTypes.string.isRequired
+};
+
+class Footer extends React.Component {
+  render() {
+    return (
       <div id="buttonsDiv">
         <label id="messageCorrect">Correct!</label>
         <a id="checkAnswerButton">Check answer</a>
         <a id="skipQuestionButton">Skip question</a>
       </div>
-    </div>
+    );
+  }
+};
+
+class Window extends React.Component {
+  render() {
+    return (
+      <div id="window">
+        <Header />
+        <Image />
+        <Word word={"Placeholder"}/>
+        <Footer />
+      </div>
+    );
+  }
+};
+
+function Application(props) {
+  return (
+    <Window />
   );
 };
 
-ReactDOM.render(<Application />, document.getElementsByTagName("main")[0]);
+Application.propTypes = {
+  words: PropTypes.array.isRequired
+};
+
+Application.defaultProps = {
+
+};
+
+ReactDOM.render(<Application words={wordlist}/>, document.getElementsByTagName("main")[0]);
